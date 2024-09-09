74a757c63944dfc10f591fa1dc6108faee6cd165
MingXi2333 <mingxi2048@gmail.com>
2024-09-09 11:55:23 +0800

    day01
    第一次修改

---
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+
+}
